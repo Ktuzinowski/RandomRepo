bc3417c366580704575513c12ae3161566dd2bd8
Ktuzinowski <67128477+Ktuzinowski@users.noreply.github.com>
2024-02-18 13:16:19 -0500

    Add files via upload
    Updated with correct format

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.json());
-app.use(express.static(__dirname))
-const port = process.env.PORT || 8080
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.static(__dirname));
+const submissionId = '65b1b43cab30997971ab6fb9';
+
+app.use((req, res, next) => {
+        // Set a custom header
+        res.set('X-CSE356', submissionId);
+        next();
+});
+
 app.get('/hw0/:bla/:blu', (_, res) => {
-    const filePath = path.join(__dirname, "hw0.html")
+    const filePath = path.join(__dirname, "hw0.html");
     res.sendFile(filePath)
 })
 app.get('/hw0', (_, res) => {
-    const filePath = path.join(__dirname, "hw0.html")
+    const filePath = path.join(__dirname, "hw0.html");
     res.sendFile(filePath)
 })
 app.get('/hw0/:blah', (_, res) => {
-    const filePath = path.join(__dirname, "hw0.html")
+    const filePath = path.join(__dirname, "hw0.html");
     res.sendFile(filePath)
 })
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
